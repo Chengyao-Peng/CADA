fca0f393ecbc387a2888ffd4165de17a2cc0ac1a
Chengyao Peng <peng_chengyao@qq.com>
2019-09-29 11:15:17 +0200

    update setup configuration
    update setup configuration

---
+++ src/featurematch/preprocessing/get_triples_HPO.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pandas
+import obo
+
+parser = obo.parser(open("../../../data/raw/hpo/hpo.obo"))
+
+#with open("../../../data/processed/hpo/triples_hpo.tsv", "rt") as f:
+
