b87b4f1ef5c6f752175dff9d6d421319944a3d83
Chengyao-Peng <54242785+Chengyao-Peng@users.noreply.github.com>
2020-04-20 16:10:46 +0200

    connected

--- src/CADA/triples_gene_hpo.py
+++ src/CADA/triples_gene_hpo.py
@@ -15,6 +15,13 @@ def triples_gene_hpo(frequent):
     else:
         in_file = os.path.join(DATA_DIRECTORY, 'raw', 'hpo', 'gene-hpo', 'ALL_SOURCES_ALL_FREQUENCIES_genes_to_phenotype.txt')
         out_file = os.path.join(DATA_DIRECTORY, 'processed', 'hpo', 'gene-hpo', 'genes_hpos.triples')
+        gene_list = os.path.join(DATA_DIRECTORY, 'processed', 'hpo', 'gene-hpo', 'genes.list')
+
+    with open(gene_list, 'w') as genefile:
+        df = pd.read_csv(in_file, skiprows=1, header=None, sep='\t').applymap(str)
+        for row in df.values.tolist():
+            gene = row[0].strip()
+            genefile.write(gene + '\n')
 
     with open(out_file, 'w') as outfile:
         df = pd.read_csv(in_file, skiprows=1, header=None, sep ='\t').applymap(str)
